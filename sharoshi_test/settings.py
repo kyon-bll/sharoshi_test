18bdaff54c1d5a94b9f08d7ef91a23b697bda96e
kyosuke <kyosuke.araki611@gmail.com>
2017-07-01 20:10:48 +0900

    time zone set

--- sharoshi_test/settings.py
+++ sharoshi_test/settings.py
@@ -102,11 +102,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-JP'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
